3f3316d8d41bd97a6e0075b27b27858329a7ad55
Eric Wulff <eric.wulff@cern.ch>
2021-05-31 13:57:18 +0200

    fix: clearer error messages on AssertionError in dataset classes

--- learning/datasets.py
+++ learning/datasets.py
@@ -24,14 +24,14 @@ class JEDIRAMDataset(Dataset):
             if self.size == None:
                 self.size = 630000
             else:
-                assert self.size <= max_size
+                assert self.size <= max_size, "max_size is {}, self.size is {}".format(max_size, self.size)
         else:
             split_dir = Path(data_dir) / "val"
             max_size = len(list(split_dir.glob("*JEDI*"))) * self.FILE_SIZE
             if self.size == None:
                 self.size = 260000
             else:
-                assert self.size <= max_size
+                assert self.size <= max_size, "max_size is {}, self.size is {}".format(max_size, self.size)
 
         data_list = []
         label_list = []
@@ -71,14 +71,14 @@ class JEDIDataset(Dataset):
             if self.size == None:
                 self.size = 630000
             else:
-                assert self.size <= max_size
+                assert self.size <= max_size, "max_size is {}, self.size is {}".format(max_size, self.size)
         else:
             self.split_dir = Path(data_dir) / "val"
             max_size = len(list(self.split_dir.glob("*JEDI*"))) * self.FILE_SIZE
             if self.size == None:
                 self.size = 260000
             else:
-                assert self.size <= max_size
+                assert self.size <= max_size, "max_size is {}, self.size is {}".format(max_size, self.size)
 
     def __getitem__(self, index):
         if not self.train:
@@ -96,7 +96,7 @@ class JEDIDataset(Dataset):
         i_file = i_b % self.FILE_SIZE  # index within file
 
         file_name = "jetImage_{}_150p_{}_{}_JEDI.h5".format(i_jet_image, i_low, i_high)
-        assert file_name in [str(path.name) for path in self.split_dir.glob("*JEDI*")], print(file_name)
+        assert file_name in [str(path.name) for path in self.split_dir.glob("*JEDI*")], print(file_name, i_a, i_jet_image, i_b)
         file = h5py.File(str(self.split_dir / file_name))
 
         return np.array(file["X"][i_file]), np.array(file["Y"][i_file])
