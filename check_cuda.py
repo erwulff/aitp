06f6d85bbb63328e2324cd8a688e6a904f472f94
Eric Wulff <eric.wulff@cern.ch>
2023-04-27 08:07:45 -0400

    check cuda devices and print device names

--- check_cuda.py
+++ check_cuda.py
@@ -1,3 +1,13 @@
 import torch
 
+num_gpus = torch.cuda.device_count()
+
+names = []
+for i_gpu in range(num_gpus):
+    names.append(torch.cuda.get_device_name(i_gpu))
+
+print("Checking PyTorch CUDA devices...")
 print("torch.cuda.is_available():", torch.cuda.is_available())
+print(f"Found {num_gpus} GPUs")
+print("Device names: ", names)
+print("PyTorch CUDA check complete")
